76715395d93c27cd46eee822fca9274b4a58f6d8
Arjan Gupta <arjangupta95@gmail.com>
2022-04-04 23:17:28 -0500

    Add responsive design for big screen vs mobile screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
 import './App.css';
+import { Responsive } from "./Responsive";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Plain's Paris App
-        </p>
-      </header>
+      <Responsive displayIn={["Laptop"]}>
+          <header className='App-header'>
+          <h1>Plain's Paris big screen view</h1>
+          <div>TODO: Prompt for downloading Plain's Paris mobile app</div>
+          </header>
+      </Responsive>
+      <Responsive displayIn={["Mobile"]}>
+        <>
+        <header className="App-header">
+          <p>
+            Plain's Paris Mobile App
+          </p>
+        </header>
+        </>
+      </Responsive>
     </div>
   );
 }
