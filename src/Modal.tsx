367c7fc7e115e81441f37c9f9db5aeaf124d2397
Arjan Gupta <arjangupta95@gmail.com>
2022-07-30 22:28:46 -0500

    Try to set super class Modal state from subclass

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -5,19 +5,30 @@ type ModalPropsType = {
   handleClose: () => void,
   show: boolean
 };
-type ModalStateType = {};
+type ModalStateType = {
+  displayString: string;
+};
 
 class Modal extends React.Component<ModalPropsType, ModalStateType> {
   constructor(props: ModalPropsType) {
     super(props);
+    this.state = {
+      displayString: ""
+    }
   }
-  
+
+  protected setDisplayString(text: string) {
+    this.setState({ displayString: text });
+  }
+
   render() {
     const showHideClassName = this.props.show ? "modal display-block" : "modal display-none";
+    const displayString = "HELLO";
     return (
       <div className={showHideClassName}>
         <section className="modal-main">
           <a href="#" className="close" onClick={this.props.handleClose}></a>
+          {displayString}
         </section>
       </div>
     );
@@ -28,7 +39,12 @@ export interface ReferUsModalPropsType extends ModalPropsType {};
 
 export class ReferUsModal extends Modal {
   constructor(props: ReferUsModalPropsType) {
-    super(props)
+    super(props);
+    super.setDisplayString("Refer Us!");
+  }
+
+  render() {
+    return super.render();
   }
 }
 
