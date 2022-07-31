b896510cf9f5b21b5ce816bd4dc1c9797968f993
Arjan Gupta <arjangupta95@gmail.com>
2022-07-30 22:48:32 -0500

    Revert "Try to set super class Modal state from subclass"
    This reverts commit 367c7fc7e115e81441f37c9f9db5aeaf124d2397.

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -5,30 +5,19 @@ type ModalPropsType = {
   handleClose: () => void,
   show: boolean
 };
-type ModalStateType = {
-  displayString: string;
-};
+type ModalStateType = {};
 
 class Modal extends React.Component<ModalPropsType, ModalStateType> {
   constructor(props: ModalPropsType) {
     super(props);
-    this.state = {
-      displayString: ""
-    }
   }
-
-  protected setDisplayString(text: string) {
-    this.setState({ displayString: text });
-  }
-
+  
   render() {
     const showHideClassName = this.props.show ? "modal display-block" : "modal display-none";
-    const displayString = "HELLO";
     return (
       <div className={showHideClassName}>
         <section className="modal-main">
           <a href="#" className="close" onClick={this.props.handleClose}></a>
-          {displayString}
         </section>
       </div>
     );
@@ -39,12 +28,7 @@ export interface ReferUsModalPropsType extends ModalPropsType {};
 
 export class ReferUsModal extends Modal {
   constructor(props: ReferUsModalPropsType) {
-    super(props);
-    super.setDisplayString("Refer Us!");
-  }
-
-  render() {
-    return super.render();
+    super(props)
   }
 }
 
